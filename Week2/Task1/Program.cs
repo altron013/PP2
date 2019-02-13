671fea422d16e5e4238cc1d64863ef396cc9ff53
altron <40513726+altron013@users.noreply.github.com>
2019-02-14 01:37:23 +0600

    Add files via upload

---
+++ Week2/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\git\PP2\lab1\Week2\Task1\TextFile1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string text = sr.ReadToEnd();
+
+            string reverse = string.Empty;
+            int length = text.Length - 1;
+            while (length >= 0)
+            {
+                reverse = reverse + text[length];
+                length--;
+            }
+
+
+            if (text == reverse) { Console.WriteLine("Yes"); }
+            else if (text != reverse) { Console.WriteLine("No"); }
+            sr.Close();
+            fs.Close();
+        }
+
+    }
+}
---
+++ Week2/Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_1
+{
+    public class Program
+    {
+        public static bool isPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i < n; ++i)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\git\PP2\lab1\Week2\Task2\TextFile1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            FileStream fs2 = new FileStream(@"C:\git\PP2\lab1\Week2\Task2\TextFile2.txt", FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs2);
+
+            string text = sr.ReadLine();
+            string[] arr = text.Split();
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                int x = int.Parse(arr[i]);
+                if (isPrime(x)) { sw.Write(x + " "); }
+            }
+
+            sw.Close();
+            fs2.Close();
+
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
+
